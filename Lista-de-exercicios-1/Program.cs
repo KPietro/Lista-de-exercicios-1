87b1f1c86b95edc24fee301393bffee637aaca22
KPietro <pietro.rios2008@gmail.com>
2025-08-21 21:00:41 -0300

    exercicio-1-da-lista-feito.

--- Lista-de-exercicios-1/Program.cs
+++ Lista-de-exercicios-1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Calculo da area de uma circunferencia");
+double r, a, pi;
+pi = 3.14;
+Console.WriteLine("digite o valor de raio");
+r = Double.Parse(Console.ReadLine());
+a = pi * (r*r);
+Console.WriteLine("A area da sua circunferencia é igual a:"+a);
